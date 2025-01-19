95eacc09ee09e7cc616a07860fb26c9adeb1192c
yuvic10 <133879116+yuvic10@users.noreply.github.com>
2025-01-19 19:30:45 +0200

    Update heatmap.py

--- heatmap.py
+++ heatmap.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 basket_file_url = "https://raw.githubusercontent.com/yuvic10/vis/main/basic_basket.xlsx"
 rent_file_url = "https://raw.githubusercontent.com/yuvic10/vis/main/rent.xlsx"
 fuel_file_url = "https://raw.githubusercontent.com/yuvic10/vis/main/fuel.xlsx"
-salary_file_url = "https://raw.githubusercontent.com/yuvic10/vis/main/salary.xlsx"
+salary_file_url = "https://raw.githubusercontent.com/yuvic10/vis/main/salary1.xlsx"
 
 # Load the data
 @st.cache_data
